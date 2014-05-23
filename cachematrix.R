b5d38077dd2743d2b2b950078fc3d807f15dc92a
faneyeung <faneyeung@163.com>
2014-05-23 23:03:47 +0800

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
+## [Put comments here that describe what your functions do]
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) {
+        r <- NULL
+        set <- function(y) {
+                x <<- y
+                r <<- NULL
+        }
+        get <- function() x
+        setSo <- function(so) r <<- so
+        getSo <- function() r
+        list(set = set, get = get,
+             setSo = setSo,
+             getSo = getSo)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        r <- x$getSo()
+        if(!is.null(r)) {
+                message("getting cached matrix")
+                return(r)
+        }
+        data <- x$get()
+        r <- solve(data, ...)
+        x$setSo(r)
+        r
 }
